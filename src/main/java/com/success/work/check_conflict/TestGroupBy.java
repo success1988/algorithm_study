389145d9fa505caf5d0d6e703cee3bc9e69e7d54
wangchenggong200506 <wangchenggong200506@credithc.com>
2020-11-05 16:12:26 +0800

    使用Collectors.groupingBy对List分组

---
+++ src/main/java/com/success/work/check_conflict/TestGroupBy.java
@@ -0,0 +1,46 @@
+package com.success.work.check_conflict;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @Title：测试List分组Collectors.groupingBy
+ * @Author：wangchenggong
+ * @Date 2020/11/5 16:02
+ * @Description
+ * @Version
+ */
+public class TestGroupBy {
+
+    public static void main(String[] args) {
+
+        ChannelBank cb1 = new ChannelBank();
+        cb1.setBankId(102);
+        cb1.setBizId(1);
+        cb1.setWeight(10);
+
+
+        ChannelBank cb2 = new ChannelBank();
+        cb2.setBankId(103);
+        cb2.setBizId(2);
+        cb2.setWeight(9);
+
+
+        ChannelBank cb3 = new ChannelBank();
+        cb3.setBankId(102);
+        cb3.setBizId(1);
+        cb3.setWeight(8);
+
+        List<ChannelBank> list = new ArrayList<>();
+        list.add(cb1);
+        list.add(cb2);
+        list.add(cb3);
+
+        Map<String, List<ChannelBank>> map = list.stream().collect(Collectors.groupingBy((item) -> item.getBankId() + "_" + item.getBizId()));
+        System.out.println(map);
+
+
+    }
+}
